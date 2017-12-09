ce85dafc92134ef6930f39a08d8158acddc2ead2
Diane Taylor <diane@rubica.com>
2017-12-08 17:56:12 -0800

    working toward intertask communication

--- schizophrenia/manager.py
+++ schizophrenia/manager.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
+import queue
+import sys
 import threading
 import types
 import time
 
+MANAGER = None
+
+def find_manager():
+    global MANAGER
+
+    return MANAGER
+
+def set_manager(manager):
+    if not isinstance(manager, Manager):
+        raise ValueError('manager must be a Manager object')
+
+    global MANAGER
+
+    MANAGER = manager
+
+from schizophrenia.result import Result
 from schizophrenia.task import Task
 
-__all__ = ['PID', 'Manager']
+__all__ = ['ClosedPipeError', 'PID', 'Pipe', 'PipeEnd', 'Manager', 'find_manager', 'set_manager']
+
+class ClosedPipeError(Exception):
+    pass
 
 class PID(object):
     def __init__(self, manager_obj, pid):
@@ -34,7 +55,7 @@ class PID(object):
         self.task.join(timeout)
 
     @classmethod
-    def join_all(self, *pids):
+    def join_all(klass, *pids):
         for pid in pids:
             if pid is None:
                 continue
@@ -43,20 +64,46 @@ class PID(object):
                 raise ValueError('pid must be a PID object')
 
         for pid in pids:
-            print('Joining PID {}'.format(pid))
+            if pid is None:
+                continue
+            
+            if not pid.is_alive():
+                continue
+            
             pid.join()
 
-    def kill(self):
+    def kill(self, exception=None):
         if self.task is None:
             raise RuntimeError('no task to kill')
 
-        self.task.kill()
+        self.task.kill(exception)
 
-    def get(self):
+    def die(self, exception=None):
+        if self.task is None:
+            raise RuntimeError('no task to kill')
+
+        self.task.die(exception)
+
+    def get(self, blocking=True, timeout=None):
         if self.task is None:
             raise RuntimeError('no task to retrieve return value')
 
-        return self.task.get()
+        return self.task.get(blocking=blocking, timeout=timeout)
+
+    @classmethod
+    def get_all(klass, *pids, blocking=True, timeout=None):
+        tasks = list()
+        
+        for pid in pids:
+            if pid is None:
+                continue
+
+            if not isinstance(pid, PID):
+                raise ValueError('pid must be a PID object')
+
+            tasks.append(pid.task)
+
+        return Task.get_all(*tasks, blocking=blocking, timeout=timeout)
 
     def __hash__(self):
         return hash(self.pid)
@@ -72,21 +119,67 @@ class PID(object):
             return '<PID:{}>'.format(self.pid)
         else:
             return '<PID:{} {}>'.format(self.pid, self.task.thread_name)
+        
+class Pipe(object):
+    def __init__(self, pid_one, pid_two):
+        self.open(pid_one, pid_two)
+
+    def open(self, pid_one, pid_two):
+        if not isinstance(pid_one, PID) or not isinstance(pid_two, PID):
+            raise ValueError('pid must be a PID object')
+
+        self.one = PipeEnd()
+        self.two = PipeEnd(send=self.one.recv, recv=self.one.send)
+        self.key = frozenset([pid_one, pid_two])
+        self.mapping = {pid_one: self.one, pid_two: self.two}
+
+    def close(self):
+        self.one.send = None
+        self.one.recv = None
+        self.two.send = None
+        self.two.recv = None
+        self.key = None
+        self.mapping = None
+
+class PipeEnd(object):
+    def __init__(self, send=None, recv=None):
+        if send is None and recv is None:
+            self.send = queue.Queue()
+            self.recv = queue.Queue()
+        elif not send is None and not recv is None:
+            self.send = send
+            self.recv = recv
+        else:
+            raise ValueError('both send and recv must be None or not None simultaneously')
+
+    def put(self, item, block=False, timeout=None):
+        if self.send is None:
+            raise ClosedPipeError('the pipe is closed')
+        
+        self.send.put(item, block, timeout)
+
+    def get(self, block=False, timeout=None):
+        if self.recv is None:
+            raise ClosedPipeError('the pipe is closed')
+        
+        return self.recv.get(block, timeout)
 
 class Manager(object):
-    MAX_PID = 2 ** 32
+    MAX_PID = 2 ** 16
     
     def __init__(self):
         self.modules = dict()
         self.tasks = dict()
         self.pids = dict()
+        self.pipes = dict()
+        self.pipe_ends = dict()
+        self.pipe_waits = dict()
 
         self.pid = 0
-        self.pid_lock = threading.Lock()
-        self.pid_event = threading.Event()
+        self.pid_lock = threading.RLock()
+        self.pipe_lock = threading.RLock()
 
         self.load_module('schizophrenia')
-        # self.launch_task('schizophrenia.manager.TaskMonitor')
 
     def load_module(self, module, load_as=None):
         if '.' in module and load_as is None:
@@ -158,12 +251,7 @@ class Manager(object):
         return obj
 
     def register_task(self, task_obj):
-        print('getting pid lock')
-        import traceback
-        traceback.print_stack()
         with self.pid_lock:
-            self.pid_event.clear()
-
             if task_obj in self.tasks:
                 raise RuntimeError('task already registered with manager')
 
@@ -178,37 +266,204 @@ class Manager(object):
             self.pids[pid_obj] = task_obj
             self.tasks[task_obj] = pid_obj
 
-            self.pid_event.set()
-
         return pid_obj
+
+    def unregister_task(self, task_obj):
+        with self.pid_lock:
+            if not task_obj in self.tasks:
+                raise RuntimeError('task not registered with manager')
+
+            pid_obj = self.tasks[task_obj]
+
+            del self.pids[pid_obj]
+            del self.tasks[task_obj]
+
+        with self.pipe_lock:
+            if pid_obj in self.pipe_ends:
+                ends = self.pipe_ends[pid_obj]
+
+                for end in list(ends)[:]:
+                    self.close_pipe_end(pid_obj, end)
 
     def launch_task(self, task_obj, *args, **kwargs):
         pid_obj = self.register_task(task_obj)
         task_obj.run(*args, **kwargs)
         return pid_obj
 
-    def wait_for_pids(self):
-        if not self.pid_lock.acquire(False):
-            self.pid_event.wait()
+    def spawn_task(self, task_name, *args, **kwargs):
+        return self.launch_task(self.create_task(task_name), *args, **kwargs)
+
+    def spawn_task_after(self, timeout, taskname, *args, **kwargs):
+        task_obj = self.create_task(task_name)
+        task_obj.run_after(timeout, *args, **kwargs)
 
     def has_pid(self, pid):
-        self.wait_for_pids()
-        return pid in self.pids
+        with self.pid_lock:
+            return pid in self.pids
 
     def has_task(self, task):
-        self.wait_for_pids()
-        return task in self.tasks
+        with self.pid_lock:
+            return task in self.tasks
 
     def get_pid(self, task):
-        if not self.has_task(task):
-            return
+        with self.pid_lock:
+            if not self.has_task(task):
+                return
 
-        self.wait_for_pids()
-        return self.tasks[task]
+            if task in self.tasks:
+                return self.tasks[task]
+
+        return None
 
     def get_task(self, pid):
-        if not self.has_pid(pid):
-            return
+        with self.pid_lock:
+            if not self.has_pid(pid):
+                return
 
-        self.wait_for_pids()        
-        return self.pids[pid]
+            if pid in self.pids:
+                return self.pids[pid]
+
+        return None
+
+    def get_pids(self):
+        with self.pid_lock:
+            return list(self.pids.keys())
+
+    def get_tasks(self):
+        with self.pid_lock:
+            return list(self.tasks.keys())
+
+    def create_pipe(self, first, second):
+        if not isinstance(first, PID):
+            raise ValueError('expected a pid for the first argument')
+        
+        if not isinstance(second, PID):
+            raise ValueError('expected a pid for the second argument')
+
+        if not self.has_pid(first):
+            raise RuntimeError('no such pid: {}'.format(repr(first)))
+
+        if not self.has_pid(second):
+            raise RuntimeError('no such pid: {}'.format(repr(first)))
+
+        if self.has_pipe_connection(first, second):
+            print('Found pipe.')
+            return self.get_pipe(first, second)
+            
+        pipe = Pipe(first, second)
+
+        with self.pipe_lock:
+            self.pipes[pipe.key] = pipe
+
+            ends = list(pipe.key)
+            print(len(ends), pipe.key, first, second)
+            self.pipe_ends.setdefault(ends[0], set()).add(ends[1])
+            self.pipe_ends.setdefault(ends[1], set()).add(ends[0])
+
+        with self.pipe_lock:
+            if first in self.pipe_waits:
+                for_pid, result = self.pipe_waits[first]
+
+                if for_pid is None or for_pid == second:
+                    result.set(pipe.mapping[first])
+                    del self.pipe_waits[first]
+
+        with self.pipe_lock:
+            if second in self.pipe_waits:
+                print('Notifying {}'.format(second))
+                for_pid, result = self.pipe_waits[second]
+
+                if for_pid is None or for_pid == first:
+                    result.set(pipe.mapping[second])
+                    del self.pipe_waits[second]
+
+        return pipe
+
+    def has_pipe(self, pid):
+        if not isinstance(pid, PID):
+            raise ValueError('pid must be a PID object')
+
+        with self.pipe_lock:
+            return pid in self.pipe_ends
+
+    def has_pipe_connection(self, pid_one, pid_two):
+        if not isinstance(pid_one, PID):
+            raise ValueError('pid must be a PID object')
+
+        if not isinstance(pid_two, PID):
+            raise ValueError('pid must be a PID object')
+
+        with self.pipe_lock:
+            return frozenset([pid_one, pid_two]) in self.pipes
+
+    def get_pipe(self, pid_one, pid_two):
+        if not isinstance(pid_one, PID):
+            raise ValueError('pid must be a PID object')
+
+        if not isinstance(pid_two, PID):
+            raise ValueError('pid must be a PID object')
+
+        key = frozenset([pid_one, pid_two])
+
+        with self.pipe_lock:
+            if not key in self.pipes:
+                raise ValueError('no such pipe with key {}'.format(key))
+
+            return self.pipes[key]
+
+    def get_ends(self, pid):
+        with self.pipe_lock:
+            if not self.has_pipe(pid):
+                raise ValueError('no pipe end for PID {}'.format(repr(pid)))
+
+            return self.pipe_ends[pid]
+
+    def get_end(self, pid_from, pid_to):
+        if not isinstance(pid_from, PID):
+            raise ValueError('pid must be a PID object')
+
+        if not isinstance(pid_to, PID):
+            raise ValueError('pid must be a PID object')
+
+        pipe = self.get_pipe(pid_from, pid_to)
+
+        return pipe.mapping[pid_to]
+
+    def wait_for_pipe(self, pid, from_pid=None):
+        pipe_result = Result()
+        
+        with self.pipe_lock:
+            self.pipe_waits[pid] = (from_pid, pipe_result)
+
+        return pipe_result
+
+    def close_pipe_end(self, pid_from, pid_to):
+        if not isinstance(pid_from, PID):
+            raise ValueError('pid must be a PID object')
+
+        if not isinstance(pid_to, PID):
+            raise ValueError('pid must be a PID object')
+        
+        key = frozenset([pid_from, pid_to])
+        self.close_pipe(key)
+
+    def close_pipe(self, pipe_key):
+        with self.pipe_lock:
+            if not pipe_key in self.pipes:
+                raise ValueError('no such pipe with key {}'.format(pipe_key))
+
+            pipe = self.pipes[pipe_key]
+            pipe.close()
+            pid_one, pid_two = list(pipe_key)
+
+            self.pipe_ends[pid_one].remove(pid_two)
+
+            if len(self.pipe_ends[pid_one]) == 0:
+                del self.pipe_ends[pid_one]
+
+            self.pipe_ends[pid_two].remove(pid_one)
+
+            if len(self.pipe_ends[pid_two]) == 0:
+                del self.pipe_ends[pid_two]
+
+MANAGER = Manager()
--- schizophrenia/task.py
+++ schizophrenia/task.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python
 
+import queue
 import sys
 import threading
 import time
 
-__all__ = ['Result', 'Task']
+from schizophrenia.result import Result, WouldBlockError
+
+__all__ = ['TaskExit', 'TaskResult', 'Task']
 
 class TaskExit(Exception):
     pass
 
-class Result(threading._Event):
-    def __init__(self, verbose=None):
-        self.value = None
-        self.task = None
-        super(Result, self).__init__(verbose)
+class TaskResult(Result):
+    def __init__(self, task):
+        self.task = task
 
-    def set(self, value=None):
-        self.value = value
-        super(Result, self).set()
+        if not isinstance(self.task, Task):
+            raise ValueError('task must be a Task object')
+        
+        super(TaskResult, self).__init__()
 
-    def clear(self):
-        self.value = None
-        super(Result, self).clear()
-
-    def get(self, timeout=None):
+    def get(self, blocking=True, timeout=None):
         if self.task is None:
             raise RuntimeError('result not bound to a task')
+
+        if not self.task.started.is_set() and blocking:
+            raise RuntimeError('task never started, get() would hang forever')
+
+        if self.task.started.is_set() and not blocking:
+            if self.task.is_alive() or not self.is_set():
+                raise WouldBlockError('the task has not finished yet')
+
+            self.raise_exception()
+            return self.value
 
         start = time.time()
 
@@ -43,29 +51,32 @@ class Result(threading._Event):
         if not timeout is None:
             timeout -= time_delta
 
-        if self.wait(timeout):
-            if not self.task.exception is None:
-                exc, tb = self.task.exception[1], self.task.exception[2]
-                raise exc.with_traceback(tb)
-            
-            return self.value
+        if not timeout is None and timeout < 0:
+            timeout = None
 
-        raise TimeoutError('result timed out')
+        return super(TaskResult, self).get(blocking, timeout)
 
 class Task(object):
     def __init__(self, manager_obj=None):
-        from schizophrenia import manager
+        from schizophrenia.manager import Manager, find_manager
 
         self.manager = manager_obj
 
-        if not self.manager is None and not isinstance(self.manager, manager.Manager):
+        if not self.manager is None and not isinstance(self.manager, Manager):
             raise ValueError('manager must be a Manager object')
+
+        if self.manager is None:
+            self.manager = find_manager()
+
+        if self.manager is None:
+            raise ValueError('no manager could be found')
         
         self.thread = None
-        self.result = Result()
-        self.result.task = self
+        self.result = TaskResult(self)
+        self.started = threading.Event()
         self.exception = None
         self.death = threading.Event()
+        self.trigger_queue = queue.Queue()
 
     @property
     def pid(self):
@@ -79,16 +90,14 @@ class Task(object):
         return '{}#{:08x}'.format(self.__class__.__name__, id(self))
 
     def bind(self, manager_obj):
-        from schizophrenia import manager
-
         if not isinstance(manager_obj, manager.Manager):
             raise ValueError('manager must be a Manager object')
 
         self.manager = manager_obj
 
     def link(self, result):
-        if not isinstance(result, Result):
-            raise ValueError('result must be a Result object')
+        if not isinstance(result, TaskResult):
+            raise ValueError('result must be a TaskResult object')
 
         if self.is_alive():
             raise RuntimeError('cannot link to running task')
@@ -127,6 +136,10 @@ class Task(object):
         self.prepare(*args, **kwargs)
         return self.start()
 
+    def run_after(self, timeout, *args, **kwargs):
+        timer = threading.Timer(timeout, self.run, args=args, kwargs=kwargs)
+        timer.start()
+
     def join(self, timeout=None):
         if self.thread is None:
             raise RuntimeError('no thread to join')
@@ -148,23 +161,87 @@ class Task(object):
         for task in tasks:
             task.join()
 
-    def kill(self):
+    def kill(self, exception=None):
+        if exception:
+            self.exception = exception
+
         self.death.set()
 
-    def get(self):
-        return self.result.get()
+    def die(self, exception=None):
+        self.kill(exception)
+        self.join()
+
+    def is_dead(self):
+        return self.death.is_set()
+
+    def dead(self):
+        if not self.exception is None:
+            raise self.exception
+
+    def get(self, blocking=True, timeout=None):
+        return self.result.get(blocking=blocking, timeout=timeout)
+
+    @classmethod
+    def get_all(self, *tasks, blocking=True, timeout=None):
+        for task in tasks:
+            if not isinstance(task, Task):
+                raise ValueError('tasks must be Task objects')
+
+        results = list()
+        
+        for task in tasks:
+            results.append(task.get(blocking=blocking, timeout=timeout))
+
+        return results
 
     def task_runner(self, *args, **kwargs):
+        self.started.clear()
+        
         try:
+            self.task_prep(*args, **kwargs)
+            self.started.set()
             self.result.set(self.task(*args, **kwargs))
         except TaskExit:
             self.result.set()
         except:
-            self.exception = sys.exc_info()
-            self.result.set()
+            self.result.set_exception()
+            import traceback
+            traceback.print_exc()
+
+        if self.manager and self.manager.has_task(self):
+            self.manager.unregister_task(self)
+
+    def task_prep(self, *args, **kwargs):
+        return
 
     def task(self, *args, **kwargs):
         raise NotImplementedError('task undefined')
+
+    def wait_for_pipe(self, from_pid=None):
+        if not self.is_alive():
+            raise RuntimeError('cannot wait for pipe when not running')
+
+        if not self.manager:
+            raise ValueError('cannot wait for pipe without a task manager')
+
+        if from_pid is None and self.manager.has_pipe(self.pid):
+            ends = self.manager.get_ends(self.pid)
+            return self.manager.get_pipe(self.pid, list(ends)[0]).mapping[self.pid]
+        if from_pid and self.manager.has_pipe_connection(self.pid, from_pid):
+            return self.manager.get_pipe(self.pid, from_pid).mapping[self.pid]
+        else:
+            return self.manager.wait_for_pipe(self.pid, from_pid)
+
+    def wait_for_start(self, timeout=None):
+        self.started.wait(timeout)
+
+    def get_pipe(self, from_pid=None):
+        result = self.wait_for_pipe(from_pid)
+
+        if isinstance(result, Result):
+            return result.get()
+        else:
+            return result
 
     def __call__(self, *args, **kwargs):
         return self.run(*args, **kwargs)
@@ -173,4 +250,4 @@ class Task(object):
         if self.pid is None:
             return '<{}>'.format(self.thread_name)
         else:
-            return '<{}: {}>'.format(self.thread_name, repr(self.pid))
+            return '<{}: PID:{}>'.format(self.thread_name, repr(self.pid.pid))
